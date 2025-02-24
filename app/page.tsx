f4125a6459715707e6bcb99ec774b2cbb49c003f
KleonGIT <barinquekleonrakem.icta@gmail.com>
2025-02-24 09:57:35 +0800

    fix vercel app

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react"
 import Image from "next/image"
 import { motion, AnimatePresence } from "framer-motion"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
